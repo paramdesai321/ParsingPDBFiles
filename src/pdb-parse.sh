2335ae9c4dd15d645a0cb8f474ce5b215da44f8e
Param Desai <param-ridham.desai.167@my.csun.edu>
2024-07-17 12:11:37 -0700

    listing .pdb files to try a different approach on proceding with the python script

--- src/pdb-parse.sh
+++ src/pdb-parse.sh
@@ -1,7 +1,9 @@
-#!bin/bash
+#!/usr/bin/bash
 
 echo Enter Protein Identification Number: 
 read PIN
 
 
-curl -O "https://files.rcsb.org/download/$PIN.pdb"
+#curl -O "https://files.rcsb.org/download/$PIN.pdb"
+
+ls | grep .pdb
