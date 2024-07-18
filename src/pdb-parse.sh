d154301f4eed341696ee6a9ddc2b3f14c40a2491
Param Desai <param-ridham.desai.167@my.csun.edu>
2024-07-17 23:58:48 -0700

    printing details about operation in the terminal

--- src/pdb-parse.sh
+++ src/pdb-parse.sh
@@ -6,4 +6,16 @@ read PIN
 
 curl -O "https://files.rcsb.org/download/$PIN.pdb"
 
+
+cat <<EOF
+--------------------------
+ATTENTION: $PIN.pdb is now dowloaded 
+-------------------------
+EOF
 python parsing.py "$PIN"
+
+cat <<EOF
+----------------------------------------------------------------
+ATTENTION: open ATOMlines$PIN.txt to access the ATOM lines from for $PIN.pdb 
+-----------------------------------------------------------------
+EOF
